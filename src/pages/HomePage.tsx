6e69ee235e903b50c4d4ae96758e571e381c0896
cheungchunman <cheung1993@gmail.com>
2021-06-26 23:14:41 +0800

    App

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
-import {Layout} from 'antd';
+import {Col, Image, Layout, Row} from 'antd';
+import img from '../assets/img.png';
 const { Content } = Layout;
 
 const Wrapper = styled.div`
-  height: 100%;
+  height: 90%;
   display: flex;
   flex-direction: column;
   padding: 16px 16px;
+  .borderNone .ant-select-selector {
+    border: none !important;
+  }
+`;
+
+const AndroidWrapper = styled.div`
+  height: 0%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 16px 16px;
+  font-family: 'Roboto', sans-serif;
+    text-align: center;
+    vertical-align: middle;
+    line-height: 90px;
+    margin-top: 90px;
   .borderNone .ant-select-selector {
     border: none !important;
   }
@@ -19,8 +37,14 @@ export default function ConvertPage() {
           <Layout>
               <Content>
                   <div className="contentWrapper">
-                      To provide you the best user interface to trade Solana SPL tokens with enthusiasm.
+                      To provide you the best user interface to trade Solana SPL tokens with enthusiasm.<br />
                   </div>
+                  <AndroidWrapper>
+                      <Row>
+                          <Col span={24}><h2><a href={"http://app.appsgeyser.com/14000580/Samoyed%20Lover%20DEX"} target="_blank" rel="noopener noreferrer" >Download Andriod App</a></h2></Col>
+                          <Col span={24}><Image preview={false}  src={img}></Image></Col>
+                      </Row>
+                  </AndroidWrapper>
               </Content>
           </Layout>
       </Wrapper>
